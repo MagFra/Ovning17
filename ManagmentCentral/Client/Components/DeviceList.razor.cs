0c7b588de98708e95b4380f415eef8b6dc74434a
MagFra <47413425+magfra@users.noreply.github.com>
2023-12-19 10:17:52 +0100

    Add project files.

---
+++ ManagmentCentral/Client/Components/DeviceList.razor.cs
@@ -0,0 +1,20 @@
+using DeviceManager.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ManagmentCentral.Client.Components
+{
+    public partial class DeviceList
+    {
+        public List<Device> Devices { get; set; } = new List<Device>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Devices.Add(new Device() { DeviceId = Guid.NewGuid(), Location = Location.Sweden, Date = new DateTime(), DeviceType = "DeviceType1", Status = Status.online });
+
+            Devices.Add(new Device() { DeviceId = Guid.NewGuid(), Location = Location.England, Date = new DateTime(), DeviceType = "DeviceType2", Status = Status.offline });
+        }
+
+    }
+}
